9e9f236c3b57d89c9ef499a43101f348cf307b73
ahmad_fadilah <v-ahmad_fadilah@linkaja.id>
2023-05-30 08:17:00 +0700

    smptp sender email

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "os"
+
+    "gopkg.in/yaml.v2"
+)
+
+// Global SMTP server configuration variable
+var smtpConfig SMTPConfig
+
+// Load SMTP server configuration from the YAML file
+func loadSMTPConfig(filename string) error {
+    data, err := os.ReadFile(filename)
+    if err != nil {
+        return err
+    }
+
+    err = yaml.Unmarshal(data, &smtpConfig)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func main() {
+
+    err := loadSMTPConfig("config.yaml")
+    if err != nil {
+        log.Fatalf("Failed to load SMTP configuration: %v", err)
+    }
+
+    // Define the endpoint for sending the email
+    http.HandleFunc("/api/book", SendEmailHandler)
+
+    // Start the HTTP server
+    log.Println("Server listening on http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
